209326d4f22419272e649dd779fe33369779c648
Vitalii Sashyn <sashyn.v@gmail.com>
2024-03-10 21:21:31 +0100

    add bubble sort

---
+++ src/algorithm/__tests__/bubbleSort.spec.js
@@ -0,0 +1,16 @@
+const sort = require("../bubbleSort").sort;
+
+test("1", () => {
+  const a = [1, 2, 3];
+  expect(a).toEqual(sort(a));
+});
+
+test("2", () => {
+  const a = [5, 2, 3];
+  expect([2, 3, 5]).toEqual(sort(a));
+});
+
+test("3", () => {
+  const a = [10, 0, -1, 12, 5, 2, 3];
+  expect([-1, 0, 2, 3, 5, 10, 12]).toEqual(sort(a));
+});
---
+++ src/algorithm/bubbleSort.js
@@ -0,0 +1,15 @@
+const sort = (arr) => {
+  for(let i = 0; i < arr.length; i++) {
+    for (let j = i+1; j < arr.length; j++) {
+      if (arr[i]>arr[j]){
+        let temp = arr[j]
+        arr[j] = arr[i]
+        arr[i] = temp
+      }
+    }
+  }
+  return arr
+}
+module.exports = {
+  sort
+}
